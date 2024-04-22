6ecd98182e6e733339c1a6aab1c45642b1b494c8
zxn <2889062858@qq.com>
2024-04-23 02:03:04 +0800

    Name->computed学习

--- vue学习/hello-v/src/App.vue
+++ vue学习/hello-v/src/App.vue
@@ -1,13 +1,15 @@
 <template>
-  <person/>
-  <game/>
+  <!-- <person/> -->
+  <!-- <game/> -->
+  <Name/>
 </template>
 
 <script>
   import Person from './components/Person.vue'
   import Game from './components/Game.vue'
+  import Name from './components/Name.vue'
   export default{
-  components:{Person, Game}//注册组件
+  components:{Person, Game, Name}//注册组件
 }
 </script>
 
---
+++ vue学习/hello-v/src/components/Name.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="perspn">
+    姓：<input type="text" v-model="firstName"><br>
+    名：<input type="text" v-model="lastName"><br>
+    全名：<span>{{ fullName }}</span>
+  </div>
+</template>
+
+<script lang="ts">
+  export default {
+    name:'Name'
+  }
+</script>
+
+<script lang="ts" setup>
+import {ref,computed } from 'vue';
+
+const firstName = ref('zhang')
+const lastName = ref('san')
+//需求：获取firstname之后将首字母大写并拼写出全名
+let fullName = computed(()=>{
+  return firstName.value.slice(0,1).toUpperCase()+firstName.value.slice(1)+'-'+lastName.value
+})
+</script>
+
+<style>
+
+</style>
