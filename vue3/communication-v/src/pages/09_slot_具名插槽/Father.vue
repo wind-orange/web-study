144f90cd8ae81a2b8d312c0266395ac392ac6a88
zxn <2889062858@qq.com>
2024-05-01 17:03:40 +0800

    具名插槽

--- vue3/communication-v/src/pages/09_slot_具名插槽/Father.vue
+++ vue3/communication-v/src/pages/09_slot_具名插槽/Father.vue
@@ -2,6 +2,35 @@
   <div class="father">
     <h3>父组件</h3>
     <div class="content">
+      <Category>
+        <!-- 默认是v-slot="",记得改格式为v-slot: -->
+        <template v-slot:s1>
+          <ul>
+            <li v-for="g in games" :key="g.id">{{ g.name }}</li>
+          </ul>
+        </template>
+        <template v-slot:s2>
+          <h2>热门游戏列表</h2>
+        </template>
+      </Category>
+
+      <Category>
+        <template v-slot:s2>
+          <img :src="imgUrl" alt="">
+        </template>
+        <template v-slot:s1>
+          <h2>今日美食城市</h2>
+        </template>
+      </Category>
+
+      <Category>
+        <template #s2>
+          <video video :src="videoUrl" controls></video>
+        </template>
+        <template #s1>
+          <h2>今日影视推荐</h2>
+        </template>
+      </Category>
 
     </div>
   </div>
@@ -9,6 +38,16 @@
 
 <script setup lang="ts" name="Father">
   import Category from './Category.vue'
+  import { ref, reactive } from "vue";
+
+  let games = reactive([
+    { id: 'asgytdfats01', name: '英雄联盟' },
+    { id: 'asgytdfats02', name: '王者农药' },
+    { id: 'asgytdfats03', name: '红色警戒' },
+    { id: 'asgytdfats04', name: '斗罗大陆' }
+  ])
+  let imgUrl = ref('https://z1.ax1x.com/2023/11/19/piNxLo4.jpg')
+  let videoUrl = ref('http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4')
 
 </script>
 
